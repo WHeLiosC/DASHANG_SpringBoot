d63e4093956d79e127f8688a6c982be5619ffc6e
yuer_98 <1042315212@qq.com>
2020-04-29 10:11:00 +0800

    cros

--- src/main/java/com/dashang/config/CorsConfig.java
+++ src/main/java/com/dashang/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.ds.demo.config;
+package com.dashang.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,20 +6,30 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+/**
+ * @author chenrui
+ * @date 2019/3/12 14:09
+ */
 @Configuration
-public class corsConfig {
+@SuppressWarnings("unused")
+public class CorsConfig {
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); // 1
-        corsConfiguration.addAllowedHeader("*"); // 2
-        corsConfiguration.addAllowedMethod("*"); // 3
+        // 1允许任何域名使用
+        corsConfiguration.addAllowedOrigin("*");
+        // 2允许任何头
+        corsConfiguration.addAllowedHeader("*");
+        // 3.允许ajax异步请求带cookie信息
+        corsConfiguration.setAllowCredentials(true);
+        // 4允许任何方法（post、get等）
+        corsConfiguration.addAllowedMethod("*");
         return corsConfiguration;
     }
 
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        source.registerCorsConfiguration("/**", buildConfig());
         return new CorsFilter(source);
     }
 }
