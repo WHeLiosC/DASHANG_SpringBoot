155137f7f5255e48f1754a8cb03bf986b1eb6fa4
yuer_98 <1042315212@qq.com>
2020-05-15 00:24:06 +0800

    uploadPic

---
+++ src/main/java/com/dashang/service/SavePicService.java
@@ -0,0 +1,28 @@
+package com.dashang.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SavePicService {
+    public static String upload(MultipartFile multipartFile) {
+        // 文件存储位置，文件的目录要存在才行，可以先创建文件目录，然后进行存储
+        String filePath = "D:/" + multipartFile.getOriginalFilename();
+        File file = new File(filePath);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // 文件存储
+        try {
+            multipartFile.transferTo(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file.getAbsolutePath();
+    }
+}
